65803fa2b052a40f85ca34f5386897ed8aa9b8a0
Monroy24 <monroy2005rdz@gmail.com>
2026-05-13 12:07:58 -0600

    chore: eliminar carpeta target y agregar .gitignore

--- target/debug/deps/mr24075P3-95c0965ca68f04af.d
+++
@@ -1,5 +0,0 @@
-C:\Users\melvi\OneDrive\Escritorio\ESDII\Parciales\mr24075P3\target\debug\deps\mr24075P3-95c0965ca68f04af.d: src\main.rs
-
-C:\Users\melvi\OneDrive\Escritorio\ESDII\Parciales\mr24075P3\target\debug\deps\mr24075P3-95c0965ca68f04af.exe: src\main.rs
-
-src\main.rs:
--- target/debug/mr24075P3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\melvi\OneDrive\Escritorio\ESDII\Parciales\mr24075P3\target\debug\mr24075P3.exe: C:\Users\melvi\OneDrive\Escritorio\ESDII\Parciales\mr24075P3\src\main.rs
